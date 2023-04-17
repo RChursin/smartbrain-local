250fa29262583898fd46d9130da49c21a6cb9b07
ganstacoder <ganstacoder@gmail.com>
2023-04-16 21:40:21 -0700

    More SmartBrain

--- src/App.js
+++ src/App.js
@@ -1,25 +1,105 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import ParticlesBg from 'particles-bg';
 import './App.css';
+import 'tachyons';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const returnClarifaiRequestOptions = (imageUrl) => {
+const PAT = 'a69ee9872baf42e29b59e1196d9b46e7';
+const USER_ID = 'ganstacoder';
+const APP_ID = 'my-first-application';
+const IMAGE_URL = imageUrl;
+
+const  raw = JSON.stringify({
+    "user_app_id": {
+        "user_id": USER_ID,
+        "app_id": APP_ID
+    },
+    "inputs": [
+        {
+            "data": {
+                "image": {
+                    "url": IMAGE_URL
+                }
+            }
+        }
+        ]
+});
+const requestOptions = {
+    method: 'POST',
+    headers: {
+        'Accept': 'application/json',
+        'Authorization': 'Key ' + PAT
+    },
+    body: raw
+};
+return requestOptions
+}
+
+
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            input: '',
+            imageUrl: '',
+            box: {}
+        };
+        this.onInputChange = this.onInputChange.bind(this);
+        this.onButtonSubmit = this.onButtonSubmit.bind(this);
+        this.calculateFaceLocation = this.calculateFaceLocation.bind(this);
+        this.displayFaceBox = this.displayFaceBox.bind(this);
+    }
+
+    calculateFaceLocation(data) {
+        const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+        const image = document.getElementById('inputimage');
+        const width = Number(image.width);
+        const height = Number(image.height);
+        return {
+            leftCol: clarifaiFace.left_col * width,
+            topRow: clarifaiFace.top_row * height,
+            rightCol: width - (clarifaiFace.right_col * width),
+            bottomRow: height - (clarifaiFace.bottom_row * height)
+        }
+    }
+
+    displayFaceBox(box) {
+        console.log(box);
+        this.setState({ box: box });
+    }
+
+    onInputChange(event) {
+        this.setState({ input: event.target.value });
+    }
+
+    onButtonSubmit() {
+        this.setState({ imageUrl: this.state.input });
+        // app.models.predict('face-detection', this.state.input)
+        fetch("https://api.clarifai.com/v2/models/" + "face-detection" + "/outputs", returnClarifaiRequestOptions(this.state.input))
+            .then(response => response.json())
+            .then(response => console.log(response))
+            .catch(error => console.log('error', error));
+    }
+
+
+    render() {
+        return (
+            <div className="App">
+                <ParticlesBg color="#FFFFFF" type="cobweb" bg={true} />
+                <Navigation />
+                <Logo />
+                <Rank />
+                <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
+                <FaceRecognition box={this.state.box} imageUrl={this.state.input} />
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('SignOut button is shown on the page', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Sign Out/i);
   expect(linkElement).toBeInTheDocument();
 });
