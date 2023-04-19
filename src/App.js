2da7709cb6d099a893bba2a31821ff3589588bbc
ganstacoder <ganstacoder@gmail.com>
2023-04-19 02:52:19 -0700

    More fixes

--- src/App.js
+++ src/App.js
@@ -66,7 +66,7 @@ class App extends Component {
     }
 
     componentDidMount() {
-        fetch("http://localhost:3001/")
+        fetch("http://localhost:3000/")
             .then(response => response.json())
             .then(console.log)
 
@@ -106,11 +106,11 @@ class App extends Component {
     onButtonSubmit() {
         this.setState({ imageUrl: this.state.input });
         if (this.state.input){
-            fetch(`https://api.clarifai.com/v2/models/face-detection/outputs`, returnClarifaiRequestOptions(this.state.input))
+            fetch("https://api.clarifai.com/v2/models/face-detection/outputs", returnClarifaiRequestOptions(this.state.input))
                 .then(response => response.json())
                 .then(response => {
                     if (response) {
-                        fetch("http://localhost:3001/image", {
+                        fetch("http://localhost:3000/image", {
                             method: 'put',
                             headers: {'Content-Type': 'application/json'},
                             body: JSON.stringify({
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,45 +1,49 @@
-import React from "react";
+import React from 'react';
 
-class Register extends React.Component{
+class Register extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            registerName: '',
-            registerEmail: '',
-            registerPassword: ''
-        }
+            email: '',
+            password: '',
+            name: ''
+        };
     }
+
     onNameChange = (event) => {
-        this.setState({registerName: event.target.value})
+        this.setState({name: event.target.value})
     }
+
     onEmailChange = (event) => {
-        this.setState({registerEmail: event.target.value})
+        this.setState({email: event.target.value})
     }
+
     onPasswordChange = (event) => {
-        this.setState({registerPassword: event.target.value})
+        this.setState({password: event.target.value})
     }
-    onSubmitRegister = () => {
-        fetch('http://localhost:3001/register', {
+
+    onSubmitSignIn = () => {
+        fetch('http://localhost:3000/register',{
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
-                name: this.state.registerName,
-                email: this.state.registerEmail,
-                password: this.state.registerPassword
+                email: this.state.email,
+                password: this.state.password,
+                name: this.state.name
             })
         })
             .then(response => response.json())
             .then(user => {
-                if (user.id) {
-                    this.props.loadUser(user);
+                if (user) {
+                    this.props.loadUser(user)
                     this.props.onRouteChange('home');
                 }
             })
     }
+
     render() {
-        const { onRouteChange } = this.props;
         return (
-            <article className="br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
+            <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
                 <main className="pa4 black-80">
                     <div className="measure">
                         <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
@@ -77,14 +81,11 @@ class Register extends React.Component{
                         </fieldset>
                         <div className="">
                             <input
-                                onClick={this.onSubmitRegister}
+                                onClick={this.onSubmitSignIn}
                                 className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
                                 type="submit"
                                 value="Register"
                             />
-                        </div>
-                        <div className="lh-copy mt3">
-                            <p onClick={() => onRouteChange('signin')} className="f6 link dim black db pointer">Sign in</p>
                         </div>
                     </div>
                 </main>
